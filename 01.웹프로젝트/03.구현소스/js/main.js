7493b8d2c566b081c1e5b25fcf3a102fba13aa5b
simda96 <sim5995@naver.com>
2023-03-16 14:46:20 +0900

    123

--- 01.웹프로젝트/03.구현소스/js/main.js
+++ 01.웹프로젝트/03.구현소스/js/main.js
@@ -11,7 +11,6 @@ window.addEventListener("DOMContentLoaded",()=>{
             let tg =
             bottomArea[idx];
             tg.style = "";
-        //    topArea.forEach(ele=>ele.style.opacity = '1');
         qwe.forEach((ele)=>{
             ele.style.opacity = 1;
         })
@@ -19,68 +18,22 @@ window.addEventListener("DOMContentLoaded",()=>{
 
         ele.onmouseenter = () => {
 
-        let tg =
-        bottomArea[idx];
-            // console.log(topArea[0])
-        // ele.style.opacity = .7;
-        tg.style.left = "0";
-        tg.style.width = "100%";
-        tg.style.opacity = "1";
-        tg.style.transition =' 1s ease-in-out, opacity 0s'
-        tg.style.animation ="big 5s ease 2s forwards"
+            let tg =
+            bottomArea[idx];
+            tg.style.left = "0";
+            tg.style.width = "100%";
+            tg.style.opacity = "1";
+            tg.style.transition =' 1s ease-in-out, opacity 0s'
+            tg.style.animation ="big 5s ease 2s forwards"
         
-        // tg.style.transition = `
-        // 1s ease-in-out,
-        // opacity 0s, transform none`;
-
-            // topArea.forEach((ele,idx)=>{
-            //     console.log(ele)
-            //     // ele.style.opacity = 1;
-            //     ele.style.opacity = 0.5;
-            //     console.log(ele[idx])
-            // })
 
             qwe.forEach((ele)=>{
                 ele.style.opacity = .7;
             })
             console.log(ele)
             ele.querySelector('.cont_tbx').style.opacity = 1;
-        // if(idx === 0){
-        //     topArea[1].style.opacity = 0.5;
-        //     topArea[2].style.opacity = 0.5;
-        //     topArea[3].style.opacity = 0.5;
-        //     topArea[4].style.opacity = 0.5;
-        // }
-        // else if(idx === 1){
-        //     topArea[2].style.opacity = 0.5;
-        //     topArea[3].style.opacity = 0.5;
-        //     topArea[4].style.opacity = 0.5;
-        //     topArea[0].style.opacity = 0.5;
-        // }
-        // else if(idx === 2){
-        //     topArea[0].style.opacity = 0.5;
-        //     topArea[1].style.opacity = 0.5;
-        //     topArea[3].style.opacity = 0.5;
-        //     topArea[4].style.opacity = 0.5;
-        // }
-        // else if(idx === 3){
-        //     topArea[0].style.opacity = 0.5;
-        //     topArea[1].style.opacity = 0.5;
-        //     topArea[2].style.opacity = 0.5;
-        //     topArea[4].style.opacity = 0.5;
-        // }
-        // else if(idx === 4){
-        //     topArea[0].style.opacity = 0.5;
-        //     topArea[1].style.opacity = 0.5;
-        //     topArea[2].style.opacity = 0.5;
-        //     topArea[3].style.opacity = 0.5;
-        // }
         
-        
-
-
-        
-
+    
         
 
       }; // 마우스 엔터 ////
