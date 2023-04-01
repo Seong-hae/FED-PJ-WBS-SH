2e382e1cf8ac5706bb43c09649b0527e8799e31e
Seong-hae <ayay1123@naver.com>
2023-04-01 20:05:53 +0900

    최선

--- 01.웹프로젝트/03.구현소스/js/main_예시.js
+++ 01.웹프로젝트/03.구현소스/js/main_예시.js
@@ -255,7 +255,164 @@ function loadFn() {
     
     **********************************************************************/
 
+    // 슬라이드 li리스트
+   let mo_slist = document.querySelectorAll(".m_slider .m_list");
     
+   // 잘라내기로 li순번이 뒤섞이므로 블릿변경 매칭을 위한
+   // 고유순번을 사용자정의 속성(data-)으로 만들어준다!
+   mo_slist.forEach((ele,idx)=>{
+       // data-seq라는 사용자정의 속성 넣기
+       ele.setAttribute("data-seq",idx);
+   }); ////// forEach ///////////
+
+   // 1. 대상선정
+   // 1-1. 이벤트 대상: .abtn
+   const mbtn = document.querySelectorAll(".mbtn");
+   // console.log(abtn);
+
+   // 1-2. 변경 대상: #slide
+   const mSlider = document.querySelector(".m_slider");
+
+   // 1-3. 블릿 대상: .indic li
+   const mindic = document.querySelectorAll(".mindic li");
+   // console.log(indic);
+
+   // 광클금지변수 : 0 - 허용, 1- 불허용
+   let protect = 0;
+
+   // 2. 슬라이드 변경함수 만들기
+   // 호출시 seq에 들어오는 값 중 1은 오른쪽, 0은 왼쪽
+   const moSlide = (seq) => {
+       // console.log("슬고우!",seq);
+
+       // console.log("못들어갔어!!!!!");
+
+       // 광클금지 설정하기 //////
+       if(protect) return;
+       protect = 1; // 잠금!
+       setTimeout(()=>{
+        protect = 0; // 해제!
+       }, 400); /// 0.4초후 해제! ///
+
+       // console.log("나,들어왔어!!!!");
+
+       // 0. 현재의 슬라이드 li수집하기
+       let molist = mSlider.querySelectorAll("li");
+       // clist -> current list 현재 리스트
+
+       // 1. 방향에 따른 분기
+       // 1-1. 오른쪽버튼 클릭시 ////////////////
+       if(seq){            
+           // console.log("오른!");
+           // (1) 오른쪽 버튼 클릭시 다음 슬라이드가
+           //     나타나도록 슬라이드 박스의 left값을
+           //     -330%로 변경시킨다.
+           mSlider.style.left = "-300%";
+           mSlider.style.transition = "left .4s ease-in-out";
+   
+           // (2) 슬라이드 이동후!!! (0.4초후)
+           setTimeout(()=>{
+               // (2-1) 바깥에 나가있는 첫번째 슬라이드
+               //       li를 잘라서 맨뒤로 보낸다!
+               mSlider.appendChild(molist[0]);
+               // (2-2) 동시에 left값을 -220%으로 변경한다!
+               mSlider.style.left = "-200%";
+               // (2-3) 트랜지션 없애기!
+               mSlider.style.transition = "none";
+           },400); //// 타임아웃 //////
+
+       }
+       // 1-2. 왼쪽버튼 클릭시 /////////////
+       else{
+           // console.log("왼쪽!");
+
+           // (1) 왼쪽버튼 클릭시 이전 슬라이드가
+           // 나타나도록 하기위해 우선 맨뒤 li를
+           // 맨앞으로 이동한다.
+           // slide.insertBefore(넣을놈,넣을놈전놈)
+           // slide.insertBefore(맨끝li,맨앞li)
+           mSlider.insertBefore(molist[molist.length-1],molist[0]);
+
+           // (2) 동시에 left값을 -330%로 변경한다.
+           mSlider.style.left = "-300%"
+           // 이때 트랜지션을 없앤다(한 번 실행 후 부터 생기므로!)
+           mSlider.style.transition = "none";
+
+           // (3) 그 후 left값을 -220%으로 애니메이션하여
+           // 슬라이드가 왼쪽에서 들어온다.
+           // 동일 속성인 left가 같은 코딩처리 공간에 동시에 있으므로
+           // 이것을 분리해야 효과가 있다
+           // setTimeout을 사용한다!
+           setTimeout(()=>{
+                mSlider.style.left = "-200%";
+                mSlider.style.transition = "left .4s ease-in-out";
+           },0); ////// 타임아웃 ///////
+           
+       } ////////////else : 왼쪽클릭시 /////////
+
+       // 2. 현재 슬라이드 순번과 같은 블릿표시하기
+       // 대상 : .indic li -> indic변수
+       // 2-1. 현재 배너리스트 업데이트
+       molist = mSlider.querySelectorAll("li");
+       // 오른쪽 클릭시(seq===1) 두번째 슬라이드[1]
+       // 왼쪽클릭시(seq===0) 첫번째 슬라이드[0]
+       // seq순번과 읽어올 슬라이드 순번이 일치한다!
+
+       // 2-2.방향별 읽어올 슬라이드 순번으로 "data-seq"값 읽어오기
+       let cseq = molist[seq].getAttribute("data-seq")
+       // console.log("현재순번:",cseq);
+
+       // 2-3. 블릿초기화
+    //    for(let x of mindic) x.classList.remove("on");
+
+       // 2-4. 읽어온 슬라이드 순번의 블릿에 클라스 "on"넣기
+    //    mindic[cseq].classList.add("on")
+
+
+   }; ////////// goSlide함수 ///////////
+
+   // 3. 대상에 이벤트 설정하기
+   mbtn.forEach((ele, idx) => {
+       ele.onclick = () => {
+           // 1. 인터발 지우기함수 호출!
+           clearAutoMo();
+           // 2. 슬라이드 함수 호출!
+           moSlide(idx);
+       }; ///// click함수 //////
+   }); /////// forEach //////////
+
+
+   // 인터발함수 지우기위한 변수설정
+   let autoIm;
+   // 타임아웃함수 지우기위한 변수설정
+   let autoTm;
+
+   /*************************************************
+       함수명: autoSlide
+       기능: 인터발함수로 슬라이드함수 호출
+   *************************************************/
+   function autoSlideMo(){
+       console.log("인터발시작!");
+       // 인터발함수로 슬라이드함수 호출하기
+       autoIm = setInterval(()=>moSlide(1),3000);
+   } ////////////// autoSlide함수 ///////////////
+
+   // 자동넘김 최초호출!
+   autoSlideMo();
+
+
+   /*******************************************
+       함수명: clearAuto
+       기능: 인터발함수를 지우고 다시셋팅
+   *******************************************/
+  function clearAutoMo(){
+       console.log("인터발멈춤!");
+       clearInterval(autoIm);
+       clearTimeout(autoTm);
+
+       autoTm = setTimeout(autoSlideMo,5000);
+
+  } ///////////clearAuto 함수 /////////////////
     
 
 
