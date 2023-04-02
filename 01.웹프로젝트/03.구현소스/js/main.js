ed0c0d28c41214670e1918cf316b5c393150483c
Seong-hae <ayay1123@naver.com>
2023-04-02 16:45:01 +0900

    123

--- 01.웹프로젝트/03.구현소스/js/main.js
+++ 01.웹프로젝트/03.구현소스/js/main.js
@@ -7,6 +7,38 @@ window.addEventListener("DOMContentLoaded", loadFn);
 function loadFn() {
 
 
+    /****************************************** 
+        HEADER 영역 - 스크롤시 배경색 변경
+    ******************************************/
+    // header 영역 스크롤시 배경색, 찾기버튼 색변경 변수
+    const headerArea = document.querySelector(".header");
+    const gnbBar = document.querySelector(".gnb");
+    const hTop = document.querySelector(".h_top");
+
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 80) {
+            headerArea.style=('background-color: #21201ef2;');
+        } else {
+            headerArea.style=('background-color: none;');
+        }
+    });
+
+
+    const hambtn = document.querySelector(".hambtn");
+    const menuT = document.querySelector(".menu-trigger");
+
+    // 햄버거
+    hambtn.addEventListener("click", function() {
+        event.preventDefault();
+        menuT.classList.toggle("active");
+        gnbBar.classList.toggle("open");
+        hTop.classList.toggle("open");
+    });
+
+    
+
+
 
     /****************************************** 
         메인영역 - 페이지 열리는 기능 구현
