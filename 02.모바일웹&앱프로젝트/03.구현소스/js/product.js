13025d8c907db500d35a9d743d30359e88933b7c
Seong-hae <ayay1123@naver.com>
2023-05-16 00:02:26 +0900

    123

--- 02.모바일웹&앱프로젝트/03.구현소스/js/product.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/product.js
@@ -13,31 +13,62 @@ function loadFn() {
     /****************************************** 
         기능: 상세상품 세부 이미지 슬라이드
     ******************************************/
-        let swiper = new Swiper(".mySwiper", {
-            loop: true,
-            spaceBetween: 10,
-            slidesPerView: 4,
-            freeMode: true,
-            watchSlidesProgress: true,
-          });
-          let swiper2 = new Swiper(".mySwiper2", {
-            loop: true,
-            spaceBetween: 10,
-            navigation: {
-              nextEl: ".swiper-button-next",
-              prevEl: ".swiper-button-prev",
+    const swiper = new Swiper(".product_slide1", {
+        loop: true,
+        spaceBetween: 10,
+        slidesPerView: 6,
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
+
+    const swiper2 = new Swiper(".product_slide2", {
+        loop: true,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: swiper,
+        },
+    });
+
+
+
+    /***************************************************
+      비슷한 상품 Slide 영역 - 갯수별 슬라이드 박스
+    ***************************************************/
+    // 3개일때
+    const swiper3 = new Swiper(".mySwiper3", {
+        slidesPerView: 3,
+        spaceBetween: 50,
+        centeredSlides: false,
+        breakpoints: {
+            //반응형 조건 속성
+            0: {
+                slidesPerView: 1,
             },
-            thumbs: {
-              swiper: swiper,
+            500: {
+                slidesPerView: 1,
             },
-          });
+            800: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            // 버튼
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 
-    
 
 
-    
-
-    
-}; ////////// load ////////////////////////
-
-
+} ////////// load ////////////////////////
