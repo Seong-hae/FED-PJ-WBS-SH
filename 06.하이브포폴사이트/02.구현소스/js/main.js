a15d9a98817a5f9699058a6097a67f3cf7fd79f6
Seong-hae <ayay1123@naver.com>
2024-03-21 19:26:10 +0900

    12

--- 06.하이브포폴사이트/02.구현소스/js/main.js
+++ 06.하이브포폴사이트/02.구현소스/js/main.js
@@ -160,6 +160,55 @@ function loadFn(){
         });
 
 
+
+
+
+        /***************************************************
+        section04 - Explor heliophysics
+        오버시 움직이는 배경
+    ***************************************************/
+    // 1.대상 선정
+    // (1)이벤트 대상 : .sun04 li
+    const exploreTable = document.querySelectorAll(".sun04 .explorelist");
+    // (2)변경 대상 : .movebox
+    const moveBox = document.querySelector(".sun04 .movebox");
+    console.log(exploreTable, moveBox);
+
+    // 2.이벤트 설정하기 : mouseenter 이벤트
+    exploreTable.forEach((ele)=>{
+        ele.onmouseenter = (e)=>{
+            // 1.요소정보 알아내기
+            // (1)top 위치값
+            let etop = ele.offsetTop;
+            // : ele(대상요소)의 정보값을 알아오면 됨
+            // (2)Height값
+            let eHeight = ele.offsetHeight;
+
+            // 확인
+            // console.log("top: ", etop, "\n Height: ", eHeight);
+
+
+            // 2.움직이는 배경박스 변경하기
+            moveBox.style.top = etop+"px";
+            moveBox.style.height = eHeight+"px";
+            moveBox.style.opacity = 1;
+
+
+        }; //////////////////// mouseenter 끝 //////////////////////////
+
+        // 마우스가 나갈때 투명도 0으로 사라짐
+        ele.onmouseleave = e => {
+            // 변경대상: .movebox = moveBox변수
+            // 변경내용: opacity -> 0
+            moveBox.style.opacity = 0;
+        }; ////////////// mouseleave ////////////
+
+
+    }); /////////////////////////////// forEach() 끝 
+
+
+
+
     
 } ////////////////// loadFn 함수 //////////////
 
